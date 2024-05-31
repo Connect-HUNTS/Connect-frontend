d916c80c4d8d4702f4d89f1f3721574ae28d89b7
andrew_siemakin <148764306+andrewsiemakin@users.noreply.github.com>
2024-05-31 20:58:38 +0300

    UsersCard updated with overused components

---
+++ components/features/TypeSection/index.tsx
@@ -0,0 +1,39 @@
+// TypeSection.tsx
+import React from "react";
+import { HiOutlineRectangleStack } from "react-icons/hi2";
+import s from "./TypeSection.module.scss";
+
+interface TypeSectionProps {
+  type: string[];
+  backgroundColors: string[];
+}
+
+const TypeSection: React.FC<TypeSectionProps> = ({
+  type,
+  backgroundColors,
+}) => {
+  return (
+    <div className={s.typeSection}>
+      <h4 className={s.typeSection_header}>
+        <HiOutlineRectangleStack className="icon" />
+        Type
+      </h4>
+      <div className={s.spanSection}>
+        {type.map((typeItem, index) => (
+          <span
+            key={index}
+            className={s.typeSpan}
+            style={{
+              background:
+                backgroundColors[index % backgroundColors.length] || "#FFFFFF",
+            }}
+          >
+            {typeItem}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TypeSection;
