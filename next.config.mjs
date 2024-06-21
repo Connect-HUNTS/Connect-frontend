28eab16652b65c59ceec5caa0c1773061fc5641f
Vladyslav Hryhorovych <vladwwe24@gmail.com>
2024-06-21 01:21:01 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 // next.config.mjs
 const nextConfig = {
     images: {
-        domains: ["t.me"],
+        domains: ["t.me", "lh3.googleusercontent.com"],
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "t.me",
                 port: "",
                 pathname: "/u/**",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/a/**",
             },
         ],
     },
